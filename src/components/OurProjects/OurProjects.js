acd854354405b31515519f711c2857a78bc4d48a
hashnthapramod <hashantha1232@gmail.com>
2024-01-08 13:54:10 +0530

    animation eka

--- src/components/OurProjects/OurProjects.js
+++ src/components/OurProjects/OurProjects.js
@@ -8,7 +8,7 @@ import "./OurProjects.css";
 
 function OurProjects() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const items = [
+  const [items, setItems] = useState([
     {
       backgroundImage: `url(${backgroundImage1})`,
       name: "FirstProject",
@@ -34,45 +34,57 @@ function OurProjects() {
       name: "FifthProject",
       description: "The description about the fifth project",
     },
-  ];
+  ]);
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === items.length - 1 ? 0 : prevIndex + 1
     );
+
+    // Shift the first item to the end of the array
+    const shiftedItems = [...items.slice(1), items[0]];
+    // Update the state with the new items order
+    setItems(shiftedItems);
   };
 
   const prevSlide = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === 0 ? items.length - 1 : prevIndex - 1
     );
+
+    // Pop the last item and add it to the beginning of the array
+    const poppedItems = [items[items.length - 1], ...items.slice(0, -1)];
+    // Update the state with the new items order
+    setItems(poppedItems);
   };
 
   return (
-    <div className="container">
-      <div className="slide">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className={index === currentIndex ? "item active" : "item"}
-            style={{ backgroundImage: item.backgroundImage }}
-          >
-            <div className="content">
-              <div className="name">{item.name}</div>
-              <div className="description">{item.description}</div>
-              <button>See More</button>
+    <div className="main-container">
+      <div className="container">
+        <div className="slide">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className={index === currentIndex ? "item active" : "item"}
+              style={{ backgroundImage: item.backgroundImage }}
+            >
+              <div className="content">
+                <div className="name">{item.name}</div>
+                <div className="description">{item.description}</div>
+                <button>See More</button>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-      <div className="button">
-        <button className="prev" onClick={prevSlide}>
-          <i className="fa-solid fa-arrow-left"></i>
-        </button>
-        <button className="next" onClick={nextSlide}>
-          <i className="fa-solid fa-arrow-right"></i>
-        </button>
+        <div className="button">
+          <button className="prev" onClick={prevSlide}>
+            <i className="fa-solid fa-arrow-left"></i>
+          </button>
+          <button className="next" onClick={nextSlide}>
+            <i className="fa-solid fa-arrow-right"></i>
+          </button>
+        </div>
       </div>
     </div>
   );
