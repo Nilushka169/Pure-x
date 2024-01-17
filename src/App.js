2faf4e04cb0cf3ad1661d87f44028f7b3a277b05
Nilushka169 <s22009961@ousl.lk>
2024-01-17 23:37:41 +0530

    ribbon cards

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
-/* import PageName from './components/PageName'; */
 import AboutUs from './components/AboutUs/AboutUs';
-/* import BackToTop from './components/BackToTop';
+import BackToTop from './components/BackToTop';
 import Home from "./components/Home/Home";
-import OurProjects from './components/OurProjects/OurProjects'; */
+import OurProjects from './components/OurProjects/OurProjects';
 
 function App() {
   return (
     <div>
-{/*       <BackToTop/>
+      <BackToTop/>
       <Home/>
-      <OurProjects/> */}
+      <OurProjects/>
       <AboutUs/>
     </div>
   );
